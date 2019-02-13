3cadc5ba1e049cdd7a7e8ae6a05372c18d1b81bf
pietro.rocchio <pietro.rocchio@marconirovereto.it>
2019-02-13 12:07:08 +0100

    Completata interazione dati del footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  descrizione = 'DESCRIZIONE';
+  contatti = 'CONTATTI';
+  linkUtili = 'LINK UTILI';
+  social = 'SOCIAL';
   constructor() { }
 
   ngOnInit() {
