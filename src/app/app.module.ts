c9d17817aa3b0e9b4420bd13e121b47dda8b5a4c
Davide Alberti <davide.alberti@marconirovereto.it>
2019-02-13 12:34:10 +0100

    Aggiunto il componente list-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { CardComponent } from './card/card.component';
+import { ListCardComponent } from './list-card/list-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    ListCardComponent
   ],
   imports: [
     BrowserModule,
