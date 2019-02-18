bc97e214d02443d90152de7a51678e0668c424fc
davide.alberti <davide.alberti@marconirovereto.it>
2019-02-18 08:40:19 +0100

    Tolta la parte futile

--- src/app/list-card/card/card.model.ts
+++ src/app/list-card/card/card.model.ts
@@ -3,6 +3,7 @@ export class Card {
     nomeComune: string;
     immagine: string;
 
+    immagine_grande: string;
     descrizione: string;
     qMin: number;
     qMax: number;
@@ -11,11 +12,12 @@ export class Card {
     mappa: string;
     id: number;
 
-    constructor(_nomeLatino: string, _nomeComune: string, _immagine: string, _descrizione: string, _qMin: number, _qMax: number, _genere: string, _epiteto: string, _mappa: string, _id: number) {
+    constructor(_nomeLatino: string, _nomeComune: string, _immagine: string, _immagine_grande: string, _descrizione: string, _qMin: number, _qMax: number, _genere: string, _epiteto: string, _mappa: string, _id: number) {
         this.nomeLatino = _nomeLatino;
         this.nomeComune = _nomeComune;
         this.immagine = _immagine;
 
+        this.immagine_grande = _immagine_grande;
         this.descrizione = _descrizione;
         this.qMin = _qMin;
         this.qMax = _qMax;
