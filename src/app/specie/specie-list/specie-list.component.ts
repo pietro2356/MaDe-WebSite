50d4e9fe64ed1f7468df097841a6bde7787e1a45
pietro2356 <pietro.rocchio@marconirovereto.it>
2019-04-01 15:54:23 +0200

    Creati i componenti necessari!

---
+++ src/app/specie/specie-list/specie-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-specie-list',
+  templateUrl: './specie-list.component.html',
+  styleUrls: ['./specie-list.component.css']
+})
+export class SpecieListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
