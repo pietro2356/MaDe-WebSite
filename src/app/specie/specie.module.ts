1fb0423a25124c7250186b53740468e54b663d17
pietro2356 <pietro.rocchio@marconirovereto.it>
2019-04-03 08:08:25 +0200

    Importato e configurato modulo specie.

--- src/app/specie/specie.module.ts
+++ src/app/specie/specie.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SpecieComponent } from './specie.component';
+import { SpecieListComponent } from './specie-list/specie-list.component';
+import { SpecieItemComponent } from './specie-list/specie-item/specie-item.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { MaterialModule } from '../material.module';
+import { FlexModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SpecieComponent,
+    SpecieListComponent,
+    SpecieItemComponent,
+    SearchFormComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    SpecieComponent
   ]
 })
 export class SpecieModule { }
