88b8269ebdee9f1fa08a997089dfa47b6b6d2e9d
Jessica <jecacerby@gmail.com>
2019-05-10 12:42:26 +0200

    Add button for mobile

--- src/app/specie/specie.component.ts
+++ src/app/specie/specie.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, NgModule } from '@angular/core';
 import { DbService } from '../common/db.service';
 import { Specie } from './model/specie.model';
 import { SpecieService } from './specie.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  stagger
+} from '@angular/animations';
+import { query } from '@angular/core/src/render3';
+
 
 @Component({
   selector: 'app-specie',
   templateUrl: './specie.component.html',
-  styleUrls: ['./specie.component.css']
+  styleUrls: ['./specie.component.css'],
+  animations: []
+})
+@NgModule({
+  imports: [ BrowserModule, BrowserAnimationsModule ]
 })
 export class SpecieComponent implements OnInit {
 
