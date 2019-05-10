c11bb5eab1d5e4c365aaf640e072137ebc4f2d4c
Jessica <jecacerby@gmail.com>
2019-05-10 15:46:47 +0200

    Edit info

--- src/app/project-info/project-info.component.ts
+++ src/app/project-info/project-info.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common'
 @Component({
   selector: 'app-project-info',
   templateUrl: './project-info.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
+  }
+  gotoPage(){
+    this.location.back();
   }
 
 }
