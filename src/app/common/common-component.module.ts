9050f484bcc427528fb2388c91f05b6f8506aa9b
Jessica Cerbaro <jessica.cerbaro@marconirovereto.it>
2019-05-15 10:25:11 +0200

    footer

--- src/app/common/common-component.module.ts
+++ src/app/common/common-component.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { ProjectInfoComponent } from './project-info/project-info.component';
 import { FooterComponent } from './footer/footer.component';
 import { LegendaErbeComponent } from './legenda-erbe/legenda-erbe.component';
+import { MaterialModule } from '../material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     ProjectInfoComponent,
     FooterComponent,
     LegendaErbeComponent
+  ],
+  imports: [
+    MaterialModule,
+    FlexLayoutModule,
   ],
   exports:[    
     ProjectInfoComponent,
